0bc6378351410a58163f1280a4cb0b111b30c454
André Pires <andrebeckpires@gmail.com>
2023-08-04 15:21:57 -0300

    filter by name

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from queue import Empty
 import tkinter as tk
 import subprocess
 
@@ -6,9 +7,15 @@ def get_input():
     sid = sid_entry.get()
     server = server_entry.get()
     cookie = cookie_entry.get()
+    file_name = file_name_entry.get()
+    domain = domain_entry.get()
 
+    
     # Build the command to run the getLinks.py script with the provided arguments
-    command = f"py getLinks.py -s {sid} -w {server} -c {cookie} -f true"
+    if(file_name):
+        command = f"py getLinks.py -s {sid} -w {server} -c {cookie} -f true -o {file_name}"
+    else:
+        command = f"py getLinks.py -s {sid} -w {server} -c {cookie} -f true"
     print(command)
     # Execute the command using subprocess
     try:
@@ -25,25 +32,34 @@ root = tk.Tk()
 root.title("Input Fields")
 
 # Create labels
-sid_label = tk.Label(root, text="SID:")
-server_label = tk.Label(root, text="Server:")
-cookie_label = tk.Label(root,text="JSESSIONID")
+sid_label = tk.Label(root, text="SID:*")
+server_label = tk.Label(root, text="Server:*")
+cookie_label = tk.Label(root,text="JSESSIONID*")
+file_name_label = tk.Label(root, text="File Name")
+domain_label = tk.Label(root, text="Filter by a domain")
+
 
 # Create input fields (Entry widgets)
 sid_entry = tk.Entry(root)
 server_entry = tk.Entry(root)
 cookie_entry = tk.Entry(root)
+file_name_entry = tk.Entry(root)
+domain_entry = tk.Entry(root)
 
 # Create a button to get input
 submit_button = tk.Button(root, text="Submit", command=get_input)
 # Use grid layout to arrange the widgets
-sid_label.grid(row=0, column=0, padx=5, pady=5)
-sid_entry.grid(row=0, column=1, padx=5, pady=5)
-server_label.grid(row=1, column=0, padx=5, pady=5)
-server_entry.grid(row=1, column=1, padx=5, pady=5)
-cookie_label.grid(row=2, column=0, padx=5, pady=5)
-cookie_entry.grid(row=2, column=1, padx=5, pady=5)
-submit_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+file_name_label.grid(row=0, column=0, padx=5, pady=5)
+file_name_entry.grid(row=0, column=1, padx=5, pady=5)
+sid_label.grid(row=1, column=0, padx=5, pady=5)
+sid_entry.grid(row=1, column=1, padx=5, pady=5)
+server_label.grid(row=2, column=0, padx=5, pady=5)
+server_entry.grid(row=2, column=1, padx=5, pady=5)
+cookie_label.grid(row=3, column=0, padx=5, pady=5)
+cookie_entry.grid(row=3, column=1, padx=5, pady=5)
+domain_label.grid(row=4, column=0, padx=5, pady=5)
+domain_entry.grid(row=4, column=1, padx=5, pady=5)
+submit_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
 # Start the main event loop
 root.mainloop()
--- results.py
+++ results.py
@@ -1,12 +1,17 @@
 import sys
 import tkinter as tk
 
+def close_window():
+    root.destroy()
+
 root = tk.Tk()
 root.title("Total")
 
 links = sys.argv[1]
 
 links_label = tk.Label(root, text=links)
-
 links_label.grid(row=0, column=0, padx=5, pady=5)
+close_button = tk.Button(root, text="Close", command=close_window)
+close_button.grid(row=1, column=0, padx=5, pady=5)
+
 root.mainloop()
